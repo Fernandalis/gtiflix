294b01c8d80cb646dc8e8aca3b18be69b570c85a
Fernandalis <ferw.1612@gmail.com>
2025-10-02 10:55:45 -0300

    Criacao de models e Seeders ator e genero

---
+++ database/seeders/Atorseeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class Atorseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('atores')->insert(
+            ['name' => "Wagner Moura", 'descricao' => "Ator brasileiro", 'nacionalidade_id' => 1],
+            ['name' => "egan Fox", 'descricao' => "Muito lembrada", 'nacionalidade_id' => 2],
+        );
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Ator;
+use App\Models\Genero;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,3 +17,13 @@ route::get('/filmes/{filme}', function($filme) {
 });
 
 route::view('/conheca', 'sobre');
+
+route::get('/generos', function() {
+   $minhavariavel = Genero::all();
+   dd($minhavariavel);
+});
+
+route::get('/atores', function() {
+    $novavariavel = Ator::all();
+    dd($novavariavel);
+ });
